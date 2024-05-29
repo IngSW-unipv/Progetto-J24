f67c27da4ace2d6cc4ab97a902f480b4e866e995
giul02 <giulia.trespiolli01@universitadipavia.it>
2024-05-29 14:51:19 +0200

    changes picking part

---
+++ SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Exception/QuantityMismatchException.java
@@ -0,0 +1,8 @@
+package it.unipv.ingsfw.SmartWarehouse.Exception;
+
+public class QuantityMismatchException extends Exception {
+	
+	public QuantityMismatchException(String message) {
+		super(message);
+	}
+}
