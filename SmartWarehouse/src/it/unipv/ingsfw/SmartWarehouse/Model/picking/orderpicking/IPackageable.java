cb248b9185707b4493fc481031c0ac18149178ce
rosci01 <151362623+rosci01@users.noreply.github.com>
2024-06-04 08:50:54 +0200

    Update IPackageable.java

--- SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/picking/orderpicking/IPackageable.java
+++ SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/picking/orderpicking/IPackageable.java
@@ -1,13 +1,20 @@
 package it.unipv.ingsfw.SmartWarehouse.Model.picking.orderpicking;
-
 import java.util.List;
 
 import it.unipv.ingsfw.SmartWarehouse.Exception.ItemNotFoundException;
 import it.unipv.ingsfw.SmartWarehouse.Exception.QuantityMismatchException;
 import it.unipv.ingsfw.SmartWarehouse.Model.inventory.InventoryItem;
-
+import it.unipv.ingsfw.SmartWarehouse.Model.Shop.*;
 public interface IPackageable {
+	public List<InventoryItem> takeInfoItem();
+	public int calculateTotalSize(); 
+	public int totaldimItem();
+	public boolean tfFragility();
+	public void setSkuqtyIntegerValue(InventoryItem item, int value);	
 	public List<InventoryItem> getSkuqtyAsList();
-	public void selectItemqty(InventoryItem item, int desiredQuantity)throws ItemNotFoundException, QuantityMismatchException;
-	public void changeDate(Orderp o);
+	public void changeDate(Order o); 
+	public void selectItemqty(InventoryItem item, int desiredQuantity) throws ItemNotFoundException, QuantityMismatchException;
+
 }
+
+
