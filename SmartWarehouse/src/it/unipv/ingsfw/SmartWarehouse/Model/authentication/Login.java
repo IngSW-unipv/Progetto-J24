d7867f08c20a0ae22ab39a2caf9da676dbb1d4f9
rosci01 <151362623+rosci01@users.noreply.github.com>
2024-06-04 08:47:53 +0200

    Update Login.java

--- SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/authentication/Login.java
+++ SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/authentication/Login.java
@@ -2,61 +2,63 @@ package it.unipv.ingsfw.SmartWarehouse.Model.authentication;
 
 import javax.swing.JFrame;
 
-import com.mysql.cj.xdevapi.Client;
+
+
 
 import it.unipv.ingsfw.SmartWarehouse.Exception.EmptyFieldException;
 import it.unipv.ingsfw.SmartWarehouse.Exception.WrongFieldException;
+import it.unipv.ingsfw.SmartWarehouse.Exception.WrongOperatorException;
 import it.unipv.ingsfw.SmartWarehouse.Model.SingletonManager;
+import it.unipv.ingsfw.SmartWarehouse.Model.user.Client;
 import it.unipv.ingsfw.SmartWarehouse.Model.user.User;
 import it.unipv.ingsfw.SmartWarehouse.Model.user.operator.WarehouseOperator;
 
 
 public class Login {
-
+	
 	public Login () {
+		
 	}
 
 	public Client loginClient (String email, String password) throws EmptyFieldException, NullPointerException {
-		fieldCheck(email, password);	
-		
-		User u = SingletonManager.getInstance().getUserDAO().getClientByEmailAndPassword(email, password);
+		fieldClCheck(email, password);	
+		User u = SingletonManager.getInstance().getUserDAO().getClientByEmail(email);
 		SingletonManager.getInstance().setLoggedUser(u);
-			
 		if(u==null) {
 			throw new NullPointerException("Client not valid");
-		} 
+		}
 		return (Client)u;
 		
 	}
 
-	private void fieldCheck(String email, String password) throws EmptyFieldException {
-		
+	private void fieldClCheck(String email, String password) throws EmptyFieldException {
 		if (email.isEmpty() == true || password.isEmpty() == true) {
 			throw new EmptyFieldException();
 		}
 		
-		
 	}
 	
-	public WarehouseOperator loginOp (String id) throws NullPointerException {
-		//fieldCheck(id);	
-		
+	public WarehouseOperator loginOp (String id) throws WrongOperatorException,EmptyFieldException{
+		fieldOpCheck(id);	
 		User u = SingletonManager.getInstance().getUserDAO().getOpById(id);
 		SingletonManager.getInstance().setLoggedUser(u);
-			
 		if(u==null) {
-			throw new NullPointerException("Operator not valid");
+			throw new WrongOperatorException();
 		} 
 		return (WarehouseOperator)u;
 	}	
-		
-	/*
-	private void passwordCheck() throws WrongFieldException {
-
-		if (!u.getPassword().equals(SingletonUser.getInstance().getUserDAO().selectPassword(u))) {
-			throw new WrongFieldException();
+	private void fieldOpCheck(String id) throws EmptyFieldException {
+		if(id.isEmpty()==true) {
+			throw new EmptyFieldException();
 		}
-
 	}
-	*/
+
+	 private void passwordClCheck(String email, String password) throws WrongFieldException {
+		 	User u= SingletonManager.getInstance().getUserDAO().getClientByEmail(email);
+		 	String pw=SingletonManager.getInstance().getUserDAO().selectPassword(u);
+		 	if (pw!=password) {
+	            throw new WrongFieldException();
+	        }
+	    }
+	
 }
