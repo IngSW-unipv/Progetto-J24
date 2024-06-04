e9d9486a1b9f2671b6a601a73adbd6ebb339788d
rosci01 <151362623+rosci01@users.noreply.github.com>
2024-06-04 08:38:48 +0200

    Update Order.java

--- SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/Shop/Order.java
+++ SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/Shop/Order.java
@@ -109,5 +109,90 @@ public class Order implements IReturnable{
 		}
 		return totalQty;
 	}
+	//union of picking part
+	public List<InventoryItem> takeInfoItem() {
+        List<InventoryItem> items = new ArrayList<>();
+        for (Map.Entry<InventoryItem, Integer> entry : skuqty.entrySet()) {
+            InventoryItem item = entry.getKey();
+            items.add(item);
+        }
+        return items;
+    }
 	
+	public int calculateTotalSize() {  
+		int totalSize = 0;
+		for (Map.Entry<InventoryItem, Integer> entry : skuqty.entrySet()) {
+			InventoryItem item = entry.getKey();
+		    int quantity = entry.getValue();
+		    int itemSize = item.getDetails().getDimension(); 
+		        totalSize += itemSize * quantity;
+		    }
+		return totalSize;
+	}
+	public int totaldimItem() {
+		int totalitems=0;
+		for (int quantity : skuqty.values()) {
+			totalitems += quantity;
+		}
+		return totalitems;
+	}
+	public boolean tfFragility() {
+		for (InventoryItem item : skuqty.keySet()) {
+			if (item.getDetails().getFragility() > N) { 
+		    return true;
+		    }
+		}
+			return false;
+	}
+	
+	public void setSkuqtyIntegerValue(InventoryItem item, int value) {
+		skuqty.put(item, value);
+	}
+		
+	
+	public List<InventoryItem> getSkuqtyAsList() {
+		List<InventoryItem> itemList = new ArrayList<>();    
+		for (Map.Entry<InventoryItem, Integer> entry : skuqty.entrySet()) {
+			InventoryItem item = entry.getKey();
+		    int quantity = entry.getValue();
+	        for (int i = 0; i < quantity; i++) {
+	             itemList.add(item);
+	        }
+		}    
+		return itemList;
+	}
+	//change data after packaged the order
+	public void changeDate(Order o) {
+		PackageStrategy s=new PackageStrategy();
+		if (s.calculatePackageSizes(o)==true) {
+			LocalDate now = LocalDate.now();
+	        int intDate = (int) now.toEpochDay(); 
+	        o.setDate(intDate);
+	   }
+	} 
+	public void selectItemqty(InventoryItem item, int desiredQuantity) throws ItemNotFoundException, QuantityMismatchException {		   
+		if (skuqty.containsKey(item)) {
+		    int actualQuantity = skuqty.get(item); 
+		    if (actualQuantity != desiredQuantity) {
+		        System.out.println("Quantity mismatch for item " + item.getSku() + ". Actual quantity in order: " + actualQuantity);
+		        throw new QuantityMismatchException("Quantity mismatch for item " + item.getSku() + ". Actual quantity in order: " + actualQuantity);
+		    }
+		    // with this you can find th position of the element
+		    	for (Map.Entry<InventoryItem, Integer> entry : skuqty.entrySet()) {
+		    		if (entry.getKey().equals(item)) {
+		    			System.out.println("Item found at position: ");
+		    			item.getPos();
+		    			System.out.println("SKU: " + entry.getKey().getSku()); 
+		    		}
+		    	}
+		    } 	else {
+		        	System.out.println("Item not in the order, your items are:");
+		        	takeInfoItem();
+		        	throw new ItemNotFoundException();
+		    }
+		}
+	
+
+}
+
 }
