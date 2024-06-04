95f2db63fa13424a7cfcf98084807c1e2eb6751c
rosci01 <151362623+rosci01@users.noreply.github.com>
2024-06-04 08:33:58 +0200

    Update WrongFieldException.java

--- SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Exception/WrongFieldException.java
+++ SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Exception/WrongFieldException.java
@@ -2,20 +2,12 @@ package it.unipv.ingsfw.SmartWarehouse.Exception;
 
 
 public class WrongFieldException extends Exception{
-	
-	private static final long serialVersionUID = 1L;
+
 	private static String errorMessage = "Errata compilazione dei campi!";
 
 	public WrongFieldException() {
 		super(errorMessage);
 	}
 
-	public void showPopup() {
-		PopupManager.showPopup(errorMessage);
-	}
-	
-	public void showPopup(String s) {
-		PopupManager.showPopup(s);
-	}
 	
 }
