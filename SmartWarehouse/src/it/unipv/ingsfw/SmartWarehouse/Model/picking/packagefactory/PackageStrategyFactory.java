37faed4c4224ebde8393922c1eab108799764f6a
rosci01 <151362623+rosci01@users.noreply.github.com>
2024-06-08 17:38:50 +0200

    Update PackageStrategyFactory.java

--- SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/picking/packagefactory/PackageStrategyFactory.java
+++ SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/picking/packagefactory/PackageStrategyFactory.java
@@ -1,10 +1,11 @@
 package it.unipv.ingsfw.SmartWarehouse.Model.picking.packagefactory;
 
+import it.unipv.ingsfw.SmartWarehouse.Model.picking.orderpicking.OrderP;
 import it.unipv.ingsfw.SmartWarehouse.Model.picking.packagestrategy.PackageStrategy;
 
 public class PackageStrategyFactory {
-	public static PackageStrategy getPackageStrategy(String strategyType) {
-			return new PackageStrategy();
+	public static PackageStrategy getPackageStrategy(OrderP o) {
+			return new PackageStrategy(o);
     }
 }
 
