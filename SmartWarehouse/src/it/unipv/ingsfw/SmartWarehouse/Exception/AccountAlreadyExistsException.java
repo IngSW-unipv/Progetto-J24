2d472606110c21a06ca8a5bfe1f89f890f657b0b
rosci01 <151362623+rosci01@users.noreply.github.com>
2024-06-09 23:08:17 +0200

    Update AccountAlreadyExistsException.java

--- SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Exception/AccountAlreadyExistsException.java
+++ SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Exception/AccountAlreadyExistsException.java
@@ -6,10 +6,5 @@ public class AccountAlreadyExistsException extends Exception{
 	private static final long serialVersionUID = 1L;
 	private static String errorMessage = "Account already exists";
 
-	public AccountAlreadyExistsException() {
-		
-		super(errorMessage);
-
-	}
 	
 }
