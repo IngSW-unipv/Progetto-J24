d7787543913fc43200b99c2545ee406c8e0300f1
rosci01 <151362623+rosci01@users.noreply.github.com>
2024-06-12 11:10:03 +0200

    Update PackageStrategyFactory.java

--- SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/picking/packagefactory/PackageStrategyFactory.java
+++ SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/picking/packagefactory/PackageStrategyFactory.java
@@ -1,11 +1,19 @@
 package it.unipv.ingsfw.SmartWarehouse.Model.picking.packagefactory;
 
 import it.unipv.ingsfw.SmartWarehouse.Model.picking.orderpicking.OrderP;
-import it.unipv.ingsfw.SmartWarehouse.Model.picking.packagestrategy.PackageStrategy;
+import it.unipv.ingsfw.SmartWarehouse.Model.picking.packagestrategy.IPackageStrategy;
+import it.unipv.ingsfw.SmartWarehouse.Model.picking.packagestrategy.MultiplePackStrategy;
+import it.unipv.ingsfw.SmartWarehouse.Model.picking.packagestrategy.SinglePackStrategy;
+
 
 public class PackageStrategyFactory {
-	public static PackageStrategy getPackageStrategy(OrderP o) {
-			return new PackageStrategy(o);
-    }
+	
+	    public IPackageStrategy getPackageStrategy(OrderP o) {
+	        int totalSize = o.calculateTotalSize();
+	        if (totalSize <= 20) {
+	            return new SinglePackStrategy(o);
+	        } else {
+	            return new MultiplePackStrategy(o);
+	        }
+	    }
 }
-
