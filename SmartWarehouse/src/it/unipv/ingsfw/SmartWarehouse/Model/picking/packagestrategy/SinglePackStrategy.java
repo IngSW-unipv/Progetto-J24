f6ff0f34f297514147942de16d394f2ce261c12a
rosci01 <151362623+rosci01@users.noreply.github.com>
2024-06-14 14:36:28 +0200

    Update SinglePackStrategy.java

--- SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/picking/packagestrategy/SinglePackStrategy.java
+++ SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/picking/packagestrategy/SinglePackStrategy.java
@@ -1,13 +1,19 @@
 package it.unipv.ingsfw.SmartWarehouse.Model.picking.packagestrategy;
 
+import java.util.ArrayList;
 import java.util.List;
 import it.unipv.ingsfw.SmartWarehouse.Model.inventory.InventoryItem;
 import it.unipv.ingsfw.SmartWarehouse.Model.picking.orderpicking.OrderP;
 
 public class SinglePackStrategy implements IPackageStrategy {
 	private OrderP o;
+	public  int counts=0;
+	public  int countm = 0;
+	public int countl = 0;
+	
     public SinglePackStrategy(OrderP o) {
        this.o=o;
+      
     }
     /*
      * method for checking if a list of items contain a fragility one 
@@ -24,36 +30,55 @@ public class SinglePackStrategy implements IPackageStrategy {
      * with this i calculate the packages
      */
     public String calculatePackages() {
-         int totalsize = o.calculateTotalSize();
-         List<InventoryItem> itemList = o.getSkuqtyAsList();
-         boolean isFragile = isPackageFragile(itemList);
-         int numPacks = totalsize / maxl; 
-         if (totalsize % maxl != 0) {
-             numPacks++;   
-         }
-         
-        String packageInfo = "You need 1 package\n";
-        packageInfo = packageInfo.concat("Package is a ");
-        if (totalsize <= maxs) {
-            packageInfo = packageInfo.concat("small pack");
-        } 
-        else if (totalsize <= maxm) {
-            packageInfo = packageInfo.concat("medium pack");
-        } 
-       else if (totalsize <= maxl)
-       {
-            packageInfo = packageInfo.concat("large pack");
-       }
-       if (isFragile) {
-            packageInfo = packageInfo.concat(" and requires fragile packaging.");
-       }
-       packageInfo = packageInfo.concat(".\n");
+        int totalsize = o.calculateTotalSize();
+        List<InventoryItem> itemList = o.getSkuqtyAsList();
+        boolean isFragile = isPackageFragile(itemList);
 
-       return packageInfo;
-           
-            
+        String packageInfo = "";
+        if (totalsize <= maxs) {
+            packageInfo = packageInfo.concat("small 1");
+            counts++;
+            System.out.println("small " + counts);
+        } else if (totalsize <= maxm) {
+            packageInfo = packageInfo.concat("medium 1");
+            countm++;
+            System.out.println("medium " + countm);
+        } else if (totalsize <= maxl) {
+            packageInfo = packageInfo.concat("large 1");
+            countl++;
+            System.out.println("large " + countl);
+        }
+        if (isFragile) {
+            packageInfo = packageInfo.concat(" and requires fragile packaging.");
+        }
+        
+        return packageInfo;
     }
-    
-   
+
+    public int getCounts() {
+        return counts;
+    }
+
+    public int getCountm() {
+        return countm;
+    }
+
+    public int getCountl() {
+        return countl;
+    }
+
+    public boolean isPackageCorrect(String packageType, int quantity) {
+        switch (packageType.toLowerCase()) {
+            case "small":
+                return counts == quantity;
+            case "medium":
+                return countm == quantity;
+            case "large":
+                return countl == quantity;
+            default:
+                return false;
+        }
+    }
+
     
 }
