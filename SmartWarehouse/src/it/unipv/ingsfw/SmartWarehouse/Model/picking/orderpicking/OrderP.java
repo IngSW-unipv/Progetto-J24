c867107a8ffbad5e8c4a0c6f40fa7be55ce78ea1
rosci01 <151362623+rosci01@users.noreply.github.com>
2024-06-14 14:35:41 +0200

    Update OrderP.java

--- SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/picking/orderpicking/OrderP.java
+++ SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/picking/orderpicking/OrderP.java
@@ -2,16 +2,22 @@ package it.unipv.ingsfw.SmartWarehouse.Model.picking.orderpicking;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import it.unipv.ingsfw.SmartWarehouse.Exception.ItemNotFoundException;
+import it.unipv.ingsfw.SmartWarehouse.Exception.PackingMismatchException;
 import it.unipv.ingsfw.SmartWarehouse.Exception.QuantityMismatchException;
 import it.unipv.ingsfw.SmartWarehouse.Model.Shop.Order;
 import it.unipv.ingsfw.SmartWarehouse.Model.inventory.InventoryItem;
+import it.unipv.ingsfw.SmartWarehouse.Model.inventory.Position;
 import it.unipv.ingsfw.SmartWarehouse.Model.picking.packagefactory.PackageStrategyFactory;
 import it.unipv.ingsfw.SmartWarehouse.Model.picking.packagestrategy.IPackageStrategy;
+import it.unipv.ingsfw.SmartWarehouse.Model.picking.packagestrategy.MultiplePackStrategy;
+import it.unipv.ingsfw.SmartWarehouse.Model.picking.packagestrategy.SinglePackStrategy;
 
 public class OrderP extends Order {
 	private HashMap<InventoryItem, Integer> skuqty;
@@ -53,11 +59,31 @@ public class OrderP extends Order {
 	 *method for show the items and some details in the order
 	 */
 	public List<String> getItemDetails() {
+	    // Sort items by their position details: line, pod, bin
+	    List<Map.Entry<InventoryItem, Integer>> sortedEntries = new ArrayList<>(skuqty.entrySet());
+	    
+	    sortedEntries.sort(new Comparator<Map.Entry<InventoryItem, Integer>>() {
+	        @Override
+	        public int compare(Map.Entry<InventoryItem, Integer> entry1, Map.Entry<InventoryItem, Integer> entry2) {
+	            Position pos1 = entry1.getKey().getPos();
+	            Position pos2 = entry2.getKey().getPos();
+	            int lineComparison = pos1.getLine().compareTo(pos2.getLine());
+	            if (lineComparison != 0) {
+	                return lineComparison;
+	            }
+	            int podComparison = pos1.getPod().compareTo(pos2.getPod());
+	            if (podComparison != 0) {
+	                return podComparison;
+	            }
+	            return pos1.getBin().compareTo(pos2.getBin());
+	        }
+	    });
+
 	    List<String> itemDetailsList = new ArrayList<>();
-	    for (Map.Entry<InventoryItem, Integer> entry : skuqty.entrySet()) {
+	    for (Map.Entry<InventoryItem, Integer> entry : sortedEntries) {
 	        InventoryItem item = entry.getKey();
 	        int quantity = entry.getValue();
-	        String itemDetails = "SKU: " + item.getSku() + "," + item.getPos() + ", Quantità: " + quantity+"\n";
+	        String itemDetails = "SKU: " + item.getSku() + ", Pos: " + item.getPos().toString() + ", Quantity: " + quantity + "\n";
 	        itemDetailsList.add(itemDetails);
 	    }
 	    return itemDetailsList;
@@ -66,22 +92,29 @@ public class OrderP extends Order {
 	 * method for select a item and a quantity
 	 */
 	public void selectItemqty(String sku, int qty) throws ItemNotFoundException, QuantityMismatchException {
-	    boolean itemFound = false;
-	    for (Map.Entry<InventoryItem, Integer> entry : skuqty.entrySet()) {
-	        if (entry.getKey().getSku().equals(sku)) {
-	            itemFound = true;
-	            int actualQuantity = entry.getValue();
-	            if (actualQuantity != qty) {
-	                throw new QuantityMismatchException("Quantity mismatch for item " + sku + ". Actual quantity in order: " + actualQuantity);
-	            }
-	        
-	        }
-	    }
+        boolean itemFound = false;
+        int actualQuantity = 0;
 
-	    if (!itemFound) {
-	        throw new ItemNotFoundException("Item not found for SKU: " + sku);
-	    }
-	}
+        // Itera sugli elementi della mappa invece che sugli entry
+        for (InventoryItem item : skuqty.keySet()) {
+            if (item.getSku().equals(sku)) {
+                itemFound = true;
+                actualQuantity = skuqty.get(item); // Ottieni la quantità associata all'item trovato
+                break;
+            }
+        }
+        if (!itemFound) {
+            throw new ItemNotFoundException("Item not found for SKU: " + sku);
+        }
+        if (actualQuantity != qty) {
+            if (qty > actualQuantity) {
+                throw new QuantityMismatchException("Too many items of SKU " + sku + " inserted. Please remove " + (qty - actualQuantity) + " items.");
+            } else {
+                throw new QuantityMismatchException("Not enough items of SKU " + sku + " inserted. Please add " + (actualQuantity - qty) + " more items.");
+            }
+        }
+    }
+
 	 /*
      * method for stamp the spuLabel
      */
@@ -95,7 +128,39 @@ public class OrderP extends Order {
 		return new PackageStrategyFactory().getPackageStrategy(this);
 	}
 
+	public HashMap<String, Integer> getItemMap() {
+	    HashMap<String, Integer> itemMap = new HashMap<>();
+	    for (Map.Entry<InventoryItem, Integer> entry : skuqty.entrySet()) {
+	        itemMap.put(entry.getKey().getSku(), entry.getValue());
+	    }
+	    return itemMap;
+	}
 	
-	    
+	/*public String addAndComparePackageSize(String packageType, int quantity) {
+        IPackageStrategy strategy = calculatePack();
+        if (strategy instanceof SinglePackStrategy) {
+            SinglePackStrategy singlePackStrategy = (SinglePackStrategy) strategy;
+            singlePackStrategy.calculatePackages(); 
+            if (singlePackStrategy.isPackageCorrect(packageType, quantity)) {
+                return "Correct number of " + packageType + " packages calculated.";
+            } else {
+                return "Incorrect number of " + packageType + " packages calculated.";
+            }
+        } else if (strategy instanceof MultiplePackStrategy) {
+            MultiplePackStrategy multiplePackStrategy = (MultiplePackStrategy) strategy;
+            multiplePackStrategy.calculatePackages(); 
+            if (multiplePackStrategy.isPackageCorrect(packageType, quantity)) {
+                return "Correct number of " + packageType + " packages calculated.";
+            } else {
+                return "Incorrect number of " + packageType + " packages calculated.";
+            }
+        } else {
+            return "Unsupported package strategy: " + strategy.getClass().getSimpleName();
+        }
+    }*/
+    }
+
+
+
+
 	
-}
