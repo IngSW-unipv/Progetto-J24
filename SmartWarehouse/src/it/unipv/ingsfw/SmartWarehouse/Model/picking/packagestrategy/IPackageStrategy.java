d444c227fbf6b1665a60058d57d38fadde8210f7
rosci01 <151362623+rosci01@users.noreply.github.com>
2024-06-19 11:14:55 +0200

    Update IPackageStrategy.java

--- SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/picking/packagestrategy/IPackageStrategy.java
+++ SmartWarehouse/src/it/unipv/ingsfw/SmartWarehouse/Model/picking/packagestrategy/IPackageStrategy.java
@@ -1,6 +1,8 @@
 package it.unipv.ingsfw.SmartWarehouse.Model.picking.packagestrategy;
 
-import java.util.HashMap;
+
+import java.util.List;
+import it.unipv.ingsfw.SmartWarehouse.Model.inventory.InventoryItem;
 
 public interface IPackageStrategy {
 	public static final int maxs = 10;
@@ -8,7 +10,13 @@ public interface IPackageStrategy {
 	public static final int maxl = 20;
 	public static final int N = 3;
 	public String  calculatePackages();
+   	public boolean isPackageCorrect(String packageType, int quantity,int fr);
+    public boolean isPackageFragile(List<InventoryItem> pack);
+	public int getCountfrl();
+	public int getCountfrm();
+	public int getCountfrs();
 	public int getCounts();
-   	public int getCountm();
-        public int getCountl();
+	public int getCountm();
+	public int getCountl();
+	public int getCountPack();
 }
